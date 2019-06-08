413a16acaa30de39a943584a0578510da6ebb29e
efserg <mynog@yandex.ru>
2019-06-08 09:25:48 +0300

    Reformat code, renaming

---
+++ src/main/java/su/efremov/NateraSimpleGraph.java
@@ -0,0 +1,7 @@
+package su.efremov;
+
+public class NateraSimpleGraph {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/su/efremov/NateraSimpleGraphTest.java
@@ -0,0 +1,15 @@
+package su.efremov;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for NateraSimpleGraph.
+ */
+public class NateraSimpleGraphTest {
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
